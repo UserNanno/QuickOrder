a2b4ebd74914eac84520d6505a9f620f293dfb0c
UserNanno <juan.canecillas@unmsm.edu.pe>
2024-05-05 20:33:57 -0500

    modificado

--- QuickO/asgi.py
+++ QuickO/asgi.py
@@ -14,3 +14,9 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuickO.settings')
 
 application = get_asgi_application()
+application = get_asgi_application()
+application = get_asgi_application()
+application = get_asgi_application()
+application = get_asgi_application()
+application = get_asgi_application()
+
